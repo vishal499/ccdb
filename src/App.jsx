ba26e00b2a2ae6de569e92f716e38397e158f8bf
vishal <vishalraoo9838@gmail.com>
2023-10-18 22:52:19 +0530

    bnbn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const App = () => {
     return (<>
         <Header />
         <Hero />
+        <Footer />
 
     </>)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Footer = () =>{
+    return(<>
+
+           <div className="footer">
+               
+               <h3>| Course Categories</h3>
+               
+           </div>
+           
+
+           <div className="footerbutton">
+                
+                <a href="#" >UI/UX Design</a>
+
+                <a href="#">Web Development</a>
+
+                <a href="#">Programming</a>
+                 
+
+
+           </div>
+
+           <div className="footerbutton2">
+                
+                <a href="#" >Android Development</a>
+
+                <a href="#">IT & Software</a>
+
+                <a href="#">Game Development</a>
+                 
+
+
+           </div>
+
+
+
+
+
+
+
+
+
+
+    </>);
+
+};
+export default Footer;
